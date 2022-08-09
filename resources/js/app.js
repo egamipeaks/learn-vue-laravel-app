176378ede3361fdbec24d62877105e05838bc62f
Andrew Krzynowek <egamipeaks@gmail.com>
2022-08-09 16:06:28 -0500

    add vite and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import './bootstrap';
+import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         vue({
@@ -23,8 +20,8 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-            "@": fileURLToPath(new URL("./resources/js", import.meta.url))
+            vue: "vue/dist/vue.esm-bundler.js",
+            "@": fileURLToPath(new URL("./resources/js", import.meta.url)),
         },
     },
 });
