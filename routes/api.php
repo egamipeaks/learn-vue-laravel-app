42ccb3a27bc5b4b535dc1c67f4b2624cc0a13c9d
Andrew Krzynowek <egamipeaks@gmail.com>
2022-08-08 23:47:09 -0500

    add API route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/assignments', function (Request $request) {
+
+    $assignments = [];
+
+    return [
+        [
+            "name" => "Finish project",
+            "complete" => false,
+            "id" => 1,
+            "tag" => "math"
+        ],
+        [
+            "name" => "Another item in the list",
+            "complete" => false,
+            "id" => 2,
+            "tag" => "reading"
+        ],
+        [
+            "name" => "Third item",
+            "complete" => false,
+            "id" => 3,
+            "tag" => "math"
+        ],
+        [
+            "name" => "Final item in the list",
+            "complete" => true,
+            "id" => 4,
+            "tag" => "science"
+        ]
+    ];
+});
